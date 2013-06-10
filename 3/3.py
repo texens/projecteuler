27f10d4ddf525f8a0be77419eeab48005a87c987
Amit Shanker <amit.shanker8@gmail.com>
2013-06-11 01:34:44 +0530

    Put in the question as well along with the answer

--- 3/3.py
+++ 3/3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 # Project Euler Problem 3
-
-# Find largest prim factor of 600851475143 
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
 
 def largest_prime(n):
     p = 2
